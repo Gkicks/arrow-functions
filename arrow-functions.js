66c50540c8b435b112b203dc3f5d19aedeaa12b3
Gkicks <>
2023-11-04 13:03:20 +0000

    finish unit

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,34 @@
 
 // Vanilla JavaScript Function
 
+// function addTwoNumbers(a,b) {
+//     return a+b
+// }
+
+// const sum = addTwoNumbers(2,2)
+// console.log(sum)
+
 // Arrow Function With Parameters
+
+const addTwoNumbers = (a,b) => {
+    return a+b;
+}
+
+let sum = addTwoNumbers(2,2)
+// console.log(sum)
+
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers1 = (a,b) => a+b
+let sum1 = addTwoNumbers(2,3)
+console.log(sum1)
+
+
 // Implicit Returns
+
+const saySomething = (message) => console.log(message)
+
+saySomething("Hello!")
 
 // Returning Multiple Lines
